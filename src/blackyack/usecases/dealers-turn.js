07325c9f1bd9bdca69257fd31dc7c37256e9264c
Brian Villa <15240615@itleon.edu.mx>
2023-02-23 23:58:30 -0600

    Add changes to dealers-turn.js

--- src/blackyack/usecases/dealers-turn.js
+++ src/blackyack/usecases/dealers-turn.js
@@ -3,13 +3,13 @@ import { takeCard, controlScores, displayCard, decideWinner } from './index';
 /**
  * Executes the logic for the dealer turn
  * @param {Number} scoreDealer Example: 21
- * @param {Number} minScore Example: 20
+ * @param {Number} scorePlayer Example: 20
  * @param {Array<String>} deck Example: ['2c', '8d', 'at', 'kp']
  * @param {Array<HTMLSpanElement>} spansScores Example: [HTMLSpanElement,...]
  * @param {HTMLDivElement} divDealerCards Example: HTMLDivElement
  * @param {Array<HTMLParagraphElement>} pNamePlayers Example: [HTMLParagrapghElement,...]
  */
-export const dealersTurn = ( scoreDealer, minScore, deck, spansScores, divDealerCards, pNamePlayers ) => {
+export const dealersTurn = ( scoreDealer, scorePlayer, deck, spansScores, divDealerCards, pNamePlayers ) => {
 
     do {
 
@@ -17,12 +17,12 @@ export const dealersTurn = ( scoreDealer, minScore, deck, spansScores, divDealer
         scoreDealer = controlScores( card, scoreDealer, spansScores[0] );
         displayCard( card, divDealerCards );
 
-        if ( minScore > 21 ){
+        if ( scorePlayer > 21 ){
             break;
         }
 
-    } while ( (scoreDealer <= minScore) && ( minScore <= 21 ) );
+    } while ( (scoreDealer <= scorePlayer) && ( scorePlayer <= 21 ) );
 
-    decideWinner( scoreDealer, minScore, spansScores, pNamePlayers );
+    decideWinner( scoreDealer, scorePlayer, spansScores, pNamePlayers );
 
 };
